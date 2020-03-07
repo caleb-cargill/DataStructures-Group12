2c14cc6c6c19adb42dcd6cc8e8e7680883cda3ff
Caleb Cargill <cargilch@mail.uc.edu>
2020-03-07 14:37:04 -0500

    In class binary tree work

--- In Class Work/IntBinaryTree.h
+++ In Class Work/IntBinaryTree.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 
 
 class Node {
@@ -32,5 +33,57 @@ public:
 			curr->left = nn;
 		else
 			curr->right = nn;
+	}
+
+	int Find(int key) {
+		if (root->data == key) {
+			return root->data;
+		}
+		Node* temp = root;
+
+		while ((temp->left != nullptr && temp->data > key) || (temp->right != nullptr && temp->data < key)) {
+			if (key < temp->data)
+				temp = temp->left;
+			else
+				temp = temp->right;
+		}
+
+		if (temp->data == key)
+			return temp->data;
+
+		// throw item not found exception
+	}
+
+	int Remove(int key) {
+		Node* temp = root;
+
+		while ((temp->left != nullptr && temp->data > key) || (temp->right != nullptr && temp->data < key)) {
+			if (temp->left->data == key || temp->right->data == key)
+				break;
+
+			if (key < temp->data)
+				temp = temp->left;
+			else if (key > temp->data)
+				temp = temp->right;
+		}
+
+		int retval;
+		if (temp->data > key)
+		{
+			retval = temp->left->data;
+			delete temp->left;
+			temp->left = nullptr;
+		}
+		else {
+			retval = temp->right->data;
+			delete temp->right;
+			temp->right = nullptr;
+		}
+
+		return retval;
+	}
+
+	void Print() {
+
 	}
 };
